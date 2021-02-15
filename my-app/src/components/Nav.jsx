c75b7ac26f8a8fc7a95592786a54aba3345cf5b1
Харченко Станислав <49039456+dem1ess@users.noreply.github.com>
2021-02-16 00:49:17 +0200

    Update Nav.jsx

--- my-app/src/components/Nav.jsx
+++ my-app/src/components/Nav.jsx
@@ -31,15 +31,3 @@ const Nav = () => {
 }
 
 export default Nav;
-// <div className='content'>
-//     <img src="https://www.pics4learning.com/images/pics-banner1-1300.jpg"/>
-//     <div>ava + description</div>
-//     <div>
-//         My post
-//         <div>
-//             New posts
-//         </div>
-//         <div>Post 1</div>
-//         <div>Post 2</div>
-//     </div>
-// </div>
