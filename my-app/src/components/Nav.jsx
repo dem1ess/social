3e04d385282b822961362cf83d0cdb57e109f047
karab <karabukula56@gmail.com>
2021-02-16 00:45:17 +0200

    social

---
+++ my-app/src/components/Nav.jsx
@@ -0,0 +1,45 @@
+const Nav = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <a href="">
+                    Profile
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    Messages
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    News
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    Music
+                </a>
+            </div>
+            <div>
+                <a href="">
+                    Settings
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
+// <div className='content'>
+//     <img src="https://www.pics4learning.com/images/pics-banner1-1300.jpg"/>
+//     <div>ava + description</div>
+//     <div>
+//         My post
+//         <div>
+//             New posts
+//         </div>
+//         <div>Post 1</div>
+//         <div>Post 2</div>
+//     </div>
+// </div>
---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src="https://www.pics4learning.com/images/pics-banner1-1300.jpg"/>
+            <div>ava + description</div>
+            <div>
+                My post
+                <div>
+                    New posts
+                </div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
